868a1203014b06d05b8e8beadbc2823912f37914
Troy <troyakins99@yahoo.com>
2021-09-23 17:00:21 -0500

    added sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :hobbies
-  resources :users, only:[]
-  resources :tests
+  resources :users
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
+  # resources :tests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

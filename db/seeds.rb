28f59664d991997b35d73840a7e5be2b859d024d
Troy <troyakins99@yahoo.com>
2021-09-23 17:34:09 -0500

    heroku db reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 Test.create!(name: 'Black-Capped Chickadee', species: 'Poecile Atricapillus')
 Test.create!(name: 'Grackle', species: 'Quiscalus Quiscula')
 Test.create!(name: 'Common Starling', species: 'Sturnus Vulgaris')

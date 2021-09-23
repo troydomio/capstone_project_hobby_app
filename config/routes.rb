9e361dcb1abf42a67cfd1b7474f49c15e1a2ae4e
Troy <troyakins99@yahoo.com>
2021-09-23 17:13:27 -0500

    heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
-  # resources :tests
+  resources :tests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
